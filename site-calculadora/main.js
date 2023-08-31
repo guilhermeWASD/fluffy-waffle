5fd8368f701c828f3cdcd7d44cb5d7cda67bb4af
GUILHERME DE OLIVEIRA RODRIGUES <guilherme.orodrigue5@senacsp.edu.br>
2023-08-31 11:59:00 -0300

    Funções corrigidas e alerta adicionado

--- site-calculadora/main.js
+++ site-calculadora/main.js
@@ -2,37 +2,48 @@
 function sum(){
     var n1 = document.getElementById("n1");
     var n2 = document.getElementById("n2");
-    var res = document.getElementById("res");
-    res.innerHTML = eval(n1.value) + eval(n2.value);
+    
+    if ((n1.value > 0) && (n2.value > 0)){
+        var res = document.getElementById("res");
+        res.innerHTML = eval(n1.value) + eval(n2.value);
+    }else{
+        alert("Numeros invalidos ");
+    }
 }
 
 function sub(){
     var n1 = document.getElementById("n1");
     var n2 = document.getElementById("n2");
+    
+    if ((n1.value > 0) && (n2.value > 0)){
     var res = document.getElementById("res");
     res.innerHTML = eval(n1.value) - eval(n2.value);
+    }else{
+        alert("Numeros invalidos ");
+    }
 }
 
 function mult(){
     var n1 = document.getElementById("n1");
     var n2 = document.getElementById("n2");
+
+    if ((n1.value > 0) && (n2.value > 0)){
     var res = document.getElementById("res");
     res.innerHTML = eval(n1.value) * eval(n2.value);
-
+    }else{
+        alert("Numeros invalidos ");
+    }
 }
 
 function division(){
     var n1 = document.getElementById("n1");
     var n2 = document.getElementById("n2");
+
+    if ((n1.value > 0) && (n2.value > 0)){
     var res = document.getElementById("res");
     res.innerHTML = eval(n1.value) / eval(n2.value);
+    }else{
+        alert("Numeros invalidos ");
+    }
 
-}
-
-
-function entrada(){
-    var nome = document.getElementById("nomelogin");
-    var nometroca = document.getElementById("nometitulo");
-    nometroca.innerHTML = "nome";
-// não deu certo *****
 }
